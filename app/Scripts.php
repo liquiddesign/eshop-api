6f27a234a9d49043b953143c0b51fc204c8d5623
Petr Křehlík <pet@lqd.cz>
2022-10-21 10:25:36 +0200

    sync-database fix

--- app/Scripts.php
+++ app/Scripts.php
@@ -2,11 +2,68 @@
 
 namespace EshopApi;
 
+use Composer\Script\Event;
+use Migrator\Migrator;
 use Nette\Configurator;
+use Nette\DI\Container;
+use Nette\Utils\Strings;
 use Security\DB\Account;
+use StORM\DIConnection;
 
 class Scripts extends \Base\Scripts
 {
+	/**
+	 * Trigger as event from composer
+	 * @param \Composer\Script\Event $event Composer event
+	 */
+	public static function syncDatabase(Event $event): void
+	{
+		$arguments = $event->getArguments();
+
+		$event->getIO()->write($arguments);
+
+		$container = static::getDIContainer($arguments);
+
+		$migrator = $container->getByType(Migrator::class);
+		$sql = $migrator->dumpAlters();
+		$event->getIO()->write($sql);
+
+		if (!Strings::trim($sql)) {
+			$event->getIO()->write('Everything is synchronized. Good job!');
+
+			return;
+		}
+
+		if (!$event->getIO()->askConfirmation('Execute SQL command? (n)', false)) {
+			return;
+		}
+
+		$container->getByType(DIConnection::class)->query($sql);
+
+		$sql = $migrator->dumpAlters();
+
+		if (!Strings::trim($sql)) {
+			$event->getIO()->write('Everything is synchronized. Good job!');
+		} else {
+			$event->getIO()->writeError(' Synchronization failed!');
+		}
+	}
+
+	/**
+	 * @param array<mixed> $arguments
+	 * @return \Nette\DI\Container
+	 */
+	protected static function getDIContainer(array $arguments): Container
+	{
+		if (isset($arguments[0]) && \is_file(\dirname(__DIR__, 4) . '/' . $arguments[0])) {
+			return require_once \dirname(__DIR__, 4) . '/' . $arguments[0];
+		}
+
+		$class = isset($arguments[0]) && \class_exists($arguments[0]) ? $arguments[0] : '\EshopApi\Bootstrap';
+
+		return \method_exists($class, 'createContainer') ? $class::createContainer() : $class::boot()->createContainer();
+	}
+
 	protected static function createConfigurator(): Configurator
 	{
 		/** @var \Nette\Configurator $configurator */
