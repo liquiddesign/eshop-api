832af0ee300a251576c612b3e23cd61bc0601c94
Petr Křehlík <pet@lqd.cz>
2022-11-18 13:29:47 +0100

    update

--- app/Schema/Outputs/CustomerOutput.php
+++ app/Schema/Outputs/CustomerOutput.php
@@ -1,21 +1,27 @@
 <?php
 
+/**
+ * This file is auto-generated.
+ */
+
 declare(strict_types=1);
 
 namespace EshopApi\Schema\Outputs;
 
-use Eshop\DB\Customer;
-use LqGrAphi\Schema\BaseOutput;
-use LqGrAphi\Schema\TypeRegister;
-
-class CustomerOutput extends BaseOutput
+class CustomerOutput extends \LqGrAphi\Schema\BaseOutput implements \LqGrAphi\Schema\ClassOutput
 {
-	public function __construct(TypeRegister $typeRegister)
+	public function __construct(\LqGrAphi\Schema\TypeRegister $typeRegister)
 	{
-		$config = [
-			'fields' => $typeRegister->createOutputFieldsFromClass(Customer::class, exclude: ['account']),
-		];
+		parent::__construct([
+			'fields' => $typeRegister->createOutputFieldsFromClass(\Eshop\DB\Customer::class, exclude: ['account']),
+		]);
+	}
 
-		parent::__construct($config);
+	/**
+	 * @return class-string<\StORM\Entity>
+	 */
+	public static function getClass(): string
+	{
+		return \Eshop\DB\Customer::class;
 	}
 }
