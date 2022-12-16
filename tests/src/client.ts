98d5de2652b3417417404eff5970aeb41c52901f
Petr Krehlik <73128826+petr6@users.noreply.github.com>
2022-12-16 12:07:06 +0100

    moved tests to roiwell-api

--- tests/src/client.ts
+++
@@ -1,8 +0,0 @@
-import {ApolloClient, InMemoryCache} from '@apollo/client/core';
-
-const client = new ApolloClient({
-    uri: 'http://18.196.72.110/eshop-api/',
-    cache: new InMemoryCache(),
-});
-
-export default client;
